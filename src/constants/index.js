7b33703cdd47dce68939a5ed144136f9fcb801a6
EugineAgolla <agollaeugine@gmail.com>
2024-10-12 20:18:00 +0300

    updates feedback

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,7 @@ import {
   jobit,
   tripguide,
   threejs,
+  
 } from "../assets";
 
 export const navLinks = [
@@ -170,30 +171,8 @@ const experiences = [
 ];
 
 const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Eugine proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Eugine does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Eugine optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
+  
+  
 ];
 
 const projects = [
@@ -208,7 +187,7 @@ const projects = [
     ],
     image: carrent,
     source_code_link: "https://github.com/",
-    live_demo_link: "https://wonder-words-blog-app.vercel.app/" // Added live demo link
+    // live_demo_link: "https://wonder-words-blog-app.vercel.app/" // Added live demo link
   },
   {
     name: "Job IT",
